0e448101311fae2e0f89a7ed654b6521c2edca40
Rob Ashton <robashton@codeofrob.com>
2013-01-27 17:42:05 +0000

    New deps

--- timer.js
+++ timer.js
@@ -1,7 +1,11 @@
+var _ = require('underscore')
+
 var Timer = function(desiredFps) {
   this.timeAtLastFrame = new Date().getTime()
   this.idealTimePerFrame = 1000 / desiredFps
   this.leftover = 0
+  this.scheduled = []
+  this.ticks = 0
 }
 
 Timer.prototype = {
@@ -10,11 +14,31 @@ Timer.prototype = {
       , timeSinceLastTick = timeAtThisFrame - this.timeAtLastFrame + this.leftover
       , catchUpFrameCount = Math.floor(timeSinceLastTick / this.idealTimePerFrame)
 
-    for(var i = 0; i < catchUpFrameCount; i++)
+    for(var i = 0; i < catchUpFrameCount; i++) {
       cb()
+      this.ticks++
+      this.fireAnyScheduledEvents()
+    }
 
     this.leftover = timeSinceLastTick - (catchUpFrameCount * this.idealTimePerFrame)
     this.timeAtLastFrame = timeAtThisFrame
+  },
+  schedule: function(cb, ms) {
+    this.scheduled.push({
+      cb: cb,
+      ticks: this.ticks + Math.floor((ms / this.idealTimePerFrame))
+    })
+  },
+  fireAnyScheduledEvents: function() {
+    var remaining = []
+    for(var i =0 ; i < this.scheduled.length; i++) {
+      var item = this.scheduled[i]
+      if(item.ticks <= this.ticks)
+        item.cb()
+      else
+        remaining.push(item)
+    }
+    this.scheduled = remaining
   }
 }
 
